fca83b72695f158378e0d780dcbcdfe8eb8d46e4
balajisrinivas <balajisrinivas@users.noreply.github.com>
2015-02-04 03:48:06 -0500

    Code Indentation complete
    TODO: Code comments to be added

--- Nativeguide/src/com/balaji/android/nativeguide/SixthActivity.java
+++ Nativeguide/src/com/balaji/android/nativeguide/SixthActivity.java
@@ -14,13 +14,11 @@ import android.widget.EditText;
 
 public class SixthActivity extends Activity implements TextToSpeech.OnInitListener 
 {
-
 	public static EditText directionsEditText;
 	public static Button spkButton;
 	private TextToSpeech tts;
+	
 	@Override
-	
-	
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
@@ -29,64 +27,62 @@ public class SixthActivity extends Activity implements TextToSpeech.OnInitListen
 		tts=new TextToSpeech(this, this);
 		
 		spkButton=(Button)findViewById(R.id.btnspk);
-		
 		directionsEditText=(EditText)findViewById(R.id.directionsText);
-		
 		String temp=GMapV2Direction.DirectionString;
 		directionsEditText.setText(android.text.Html.fromHtml(temp));
-		//directionsEditText.setText("Hello");
 		
-		spkButton.setOnClickListener(new View.OnClickListener() {
-			 
-            @Override
-            public void onClick(View arg0) {
-                speakOut();
-            }
- 
-        });
+		spkButton.setOnClickListener(new View.OnClickListener() 
+		{
 		
+			@Override
+			public void onClick(View arg0) 
+			{
+				speakOut();
+			}
+		});
 	}
-
-	public void onDestroy() {
-        // Don't forget to shutdown tts!
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
-        }
-        super.onDestroy();
-    }
- 
-    @Override
-    public void onInit(int status) {
- 
-        if (status == TextToSpeech.SUCCESS) {
- 
-            int result = tts.setLanguage(Locale.US);
- 
-            if (result == TextToSpeech.LANG_MISSING_DATA
-                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Log.e("TTS", "This Language is not supported");
-            } else {
-                directionsEditText.setEnabled(true);
-                speakOut();
-            }
- 
-        } else {
-            Log.e("TTS", "Initilization Failed!");
-        }
- 
-    }
-    
-    private void speakOut()
-    {
-    	String dirtext=GMapV2Direction.DirectionString;
-    	Log.i("Dirtext",dirtext);
-    	String fintext=Html.fromHtml(dirtext).toString();
-    	Log.i("Fintext", fintext);
-        tts.speak(fintext, TextToSpeech.QUEUE_FLUSH, null);
-    }
 	
-
+	public void onDestroy() 
+	{
+		// Shutdown thw TTS engine!
+		if (tts != null) 
+		{
+			tts.stop();
+			tts.shutdown();
+		}
+		super.onDestroy();
+	}
+	
+	@Override
+	public void onInit(int status) 
+	{
+		if (status == TextToSpeech.SUCCESS) 
+		{
+			int result = tts.setLanguage(Locale.US);
+			
+			if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) 
+			{
+				Log.e("TTS", "This Language is not supported");
+			} else 
+			{
+				directionsEditText.setEnabled(true);
+				speakOut();
+			}
+		} 
+		else 
+		{
+			Log.e("TTS", "Initilization Failed!");
+		}
+	}
+	
+	private void speakOut()
+	{
+		String dirtext=GMapV2Direction.DirectionString;
+		Log.i("Dirtext",dirtext);
+		String fintext=Html.fromHtml(dirtext).toString();
+		Log.i("Fintext", fintext);
+		tts.speak(fintext, TextToSpeech.QUEUE_FLUSH, null);
+	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) 
@@ -95,5 +91,4 @@ public class SixthActivity extends Activity implements TextToSpeech.OnInitListen
 		getMenuInflater().inflate(R.menu.activity_sixth, menu);
 		return true;
 	}
-
 }
