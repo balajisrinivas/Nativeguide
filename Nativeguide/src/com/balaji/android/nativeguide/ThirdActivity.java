e91e1297bc66b88ce154887e711c7bfba375491d
balajisrinivas <balajisrinivas@users.noreply.github.com>
2015-02-04 03:51:55 -0500

    Code Indentation complete
    TODO: Code comments to be added

--- Nativeguide/src/com/balaji/android/nativeguide/ThirdActivity.java
+++ Nativeguide/src/com/balaji/android/nativeguide/ThirdActivity.java
@@ -9,14 +9,13 @@ import android.widget.RadioButton;
 
 public class ThirdActivity extends Activity 
 {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_third);
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) 
 	{
@@ -28,27 +27,26 @@ public class ThirdActivity extends Activity
 	public void onRadioButtonClicked(View view)
 	{
 		boolean checked = ((RadioButton) view).isChecked();
-        switch(view.getId()) 
-        {
-            case R.id.drivingRadio:
-                if (checked)
-                    MapFragmentActivity.MODE="driving";
-                break;
-            case R.id.walkingRadio:
-                if (checked)
-                	MapFragmentActivity.MODE="walking";
-                break;
-            case R.id.bicyclingRadio:
-                if (checked)
-                    MapFragmentActivity.MODE="bicycling";
-                break;
-            case R.id.transitRadio:
-                if (checked)
-                    MapFragmentActivity.MODE="transit";
-                break;
-        }
-        Intent intent=new Intent(this,FourthActivity.class);
-        startActivity(intent);
+		switch(view.getId()) 
+		{
+			case R.id.drivingRadio:
+				if (checked)
+					MapFragmentActivity.MODE="driving";
+				break;
+			case R.id.walkingRadio:
+				if (checked)
+					MapFragmentActivity.MODE="walking";
+				break;
+			case R.id.bicyclingRadio:
+				if (checked)
+					MapFragmentActivity.MODE="bicycling";
+				break;
+			case R.id.transitRadio:
+				if (checked)
+					MapFragmentActivity.MODE="transit";
+				break;
+		}
+		Intent intent=new Intent(this,FourthActivity.class);
+		startActivity(intent);
 	}
-
 }
